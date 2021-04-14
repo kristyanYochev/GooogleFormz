836038a117bfb79b93c1a9327c5f1602808ac0f4
Kristyan Yochev <kris.yochev@gmail.com>
2021-04-14 13:33:43 +0300

    Initial commit

---
+++ src/main/kotlin/org/elsys/ip/forms/FormsApplication.kt
@@ -0,0 +1,11 @@
+package org.elsys.ip.forms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FormsApplication
+
+fun main(args: Array<String>) {
+    runApplication<FormsApplication>(*args)
+}
---
+++ src/test/kotlin/org/elsys/ip/forms/FormsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.elsys.ip.forms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FormsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
