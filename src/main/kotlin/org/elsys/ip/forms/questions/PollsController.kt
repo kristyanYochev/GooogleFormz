451a140efe3e9859c27c786b526707bffeb779fd
Kristyan Yochev <kris.yochev@gmail.com>
2021-04-28 21:27:38 +0300

    Added responses to poll

--- src/main/kotlin/org/elsys/ip/forms/questions/PollsController.kt
+++ src/main/kotlin/org/elsys/ip/forms/questions/PollsController.kt
@@ -14,27 +14,54 @@ class PollsController(
 ) {
     @GetMapping("{id}")
     fun poll(
-            @PathVariable(name="id") id: EntityId,
+            @PathVariable(name = "id") id: EntityId,
             model: Model
     ): String {
         val poll = pollsService.getPoll(id) ?: throw EntityNotFound(id, "Poll")
-        val pollDto = PollDTO(poll)
+
+        if (!poll.open) throw UnauthorizedAccess()
+
+        model.addAttribute("poll", PollDTO(poll))
+        model.addAttribute("response", ResponseDTO(poll.id, MutableList(poll.questions.size) { -1 }))
+        return "polls/respond"
+    }
+
+    @PostMapping("{id}")
+    fun poll(
+            @PathVariable(name = "id") pollId: EntityId,
+            @ModelAttribute(name = "response") responseDTO: ResponseDTO
+    ): String {
+        val poll = pollsService.getPoll(pollId) ?: throw EntityNotFound(pollId, "Poll")
+        if (!poll.open) throw UnauthorizedAccess()
+
+        pollsService.respond(responseDTO.responses)
+
+        return "redirect:/"
+    }
+
+    @GetMapping("{id}/edit")
+    fun editPoll(
+            @PathVariable(name="id") id: EntityId,
+            model: Model
+    ): String {
+        pollsService.checkAuthor(id, usersService.currentUser()!!)
+
+        val poll = pollsService.getPoll(id) ?: throw EntityNotFound(id, "Poll")
+        val pollDto = EditPollDTO(poll)
         model.addAttribute("poll", pollDto)
         model.addAttribute("pollId", id)
 
-        return "polls/poll"
+        return "polls/edit"
     }
 
     @GetMapping("{id}/start")
     fun startPoll(@PathVariable(name = "id") id: EntityId): String {
-        val poll = pollsService.getPoll(id) ?: throw EntityNotFound(id, "Poll")
-        if (usersService.currentUser()!!.id != poll.author.id) {
-            throw UnauthorizedAccess()
-        }
+        pollsService.checkAuthor(id, usersService.currentUser()!!)
 
         pollsService.startPoll(id)
 
-        return "redirect:/polls/${id}"
+        @Suppress("SpringMVCViewInspection")
+        return "redirect:/polls/$id"
     }
 
     @GetMapping("{id}/add-question")
@@ -43,8 +70,10 @@ class PollsController(
             @RequestParam(name = "answersCount", defaultValue = "4") answersCount: Int,
             model: Model
     ): String {
+        pollsService.checkAuthor(id, usersService.currentUser()!!)
+
         model.addAttribute("answersCount", answersCount)
-        val questionDTO = QuestionDTO(answers = MutableList(answersCount) { "" })
+        val questionDTO = EditQuestionDTO(answers = MutableList(answersCount) { "" })
         model.addAttribute("question", questionDTO)
         return "polls/editQuestion"
     }
@@ -52,43 +81,86 @@ class PollsController(
     @PostMapping("{id}/add-question")
     fun addQuestion(
         @PathVariable(name = "id") id: EntityId,
-        @ModelAttribute(name = "question") questionDTO: QuestionDTO
+        @ModelAttribute(name = "question") editQuestionDTO: EditQuestionDTO
     ): String {
-        pollsService.addQuestionToPoll(id, questionDTO.question, questionDTO.answers)
-        return "redirect:/polls/${id}"
+        pollsService.checkAuthor(id, usersService.currentUser()!!)
+
+        pollsService.addQuestionToPoll(id, editQuestionDTO.question, editQuestionDTO.answers)
+
+        @Suppress("SpringMVCViewInspection")
+        return "redirect:/polls/$id"
     }
 
     @GetMapping("create")
     fun createPoll(model: Model): String {
-        model.addAttribute("poll", PollDTO())
+        model.addAttribute("poll", EditPollDTO())
         return "polls/create"
     }
 
     @PostMapping("create")
-    fun createPoll(@ModelAttribute("poll") pollDto: PollDTO): String {
-        val poll = pollsService.createEmptyPoll(pollDto.title, usersService.currentUser()!!)
+    fun createPoll(@ModelAttribute("poll") editPollDto: EditPollDTO): String {
+        val poll = pollsService.createEmptyPoll(editPollDto.title, usersService.currentUser()!!)
         return "redirect:${poll.id}"
     }
 }
 
-data class PollDTO(
+data class EditPollDTO(
         val title: String = "",
-        val questions: List<QuestionDTO> = emptyList(),
-        val open: Boolean = false
+        val questions: List<EditQuestionDTO> = emptyList(),
+        val open: Boolean = false,
 ) {
     constructor(poll: Poll): this(
             poll.title,
-            poll.questions.map { QuestionDTO(it) },
+            poll.questions.map { EditQuestionDTO(it) },
             poll.open
     )
 }
 
-data class QuestionDTO(
+data class EditQuestionDTO(
         val question: String = "",
         val answers: MutableList<String> = mutableListOf()
 ) {
-    constructor(question: Question) : this(
+    constructor(question: Question): this(
             question.question,
             question.answers.map { it.answer } .toMutableList()
     )
 }
+
+data class PollDTO(
+        val title: String,
+        val questions: List<QuestionDTO>,
+        val id: EntityId
+) {
+    constructor(poll: Poll): this(
+            poll.title,
+            poll.questions.map { QuestionDTO(it) },
+            poll.id
+    )
+}
+
+data class QuestionDTO(
+        val question: String,
+        val answers: List<AnswerDTO>,
+        val id: EntityId
+) {
+    constructor(question: Question): this(
+            question.question,
+            question.answers.map { AnswerDTO(it) },
+            question.id
+    )
+}
+
+data class AnswerDTO(
+        val answer: String,
+        val id: EntityId
+) {
+    constructor(answer: Answer): this(
+            answer.answer,
+            answer.id
+    )
+}
+
+data class ResponseDTO(
+        val pollId: EntityId = -1,
+        val responses: MutableList<EntityId> = mutableListOf()
+)
--- src/main/kotlin/org/elsys/ip/forms/questions/PollsService.kt
+++ src/main/kotlin/org/elsys/ip/forms/questions/PollsService.kt
@@ -4,14 +4,25 @@ import org.elsys.ip.forms.*
 import org.elsys.ip.forms.auth.User
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import javax.transaction.Transactional
 
 @Service
 class PollsService(
+        private val answersRepo: AnswersRepo,
         private val pollsRepo: PollsRepo
 ) {
     fun createEmptyPoll(title: String, author: User) = pollsRepo.save(Poll(title, author = author))
 
     fun getPoll(id: EntityId) = pollsRepo.findByIdOrNull(id)
+
+    fun checkAuthor(pollId: EntityId, user: User) {
+        if (!isAuthor(pollId, user)) throw UnauthorizedAccess()
+    }
+
+    fun isAuthor(pollId: EntityId, user: User): Boolean {
+        val poll = pollsRepo.findByIdOrNull(pollId) ?: throw EntityNotFound(pollId, "Poll")
+        return poll.author.id != user.id
+    }
 
     fun addQuestionToPoll(id: EntityId, question: String, answers: List<String>) {
         val questionEntity = Question(question, answers.map { Answer(it) })
@@ -37,6 +48,15 @@ class PollsService(
         maybePoll.ifPresent {
             it.open = false
             pollsRepo.save(it)
+        }
+    }
+
+    @Transactional
+    fun respond(responses: List<EntityId>) {
+        for (answerId in responses) {
+            val answer = answersRepo.findByIdOrNull(answerId) ?: throw EntityNotFound(answerId, "Answer")
+            answer.responseCount += 1
+            answersRepo.save(answer)
         }
     }
 }
